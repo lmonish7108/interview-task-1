92cf9ffbc826c92d90430328c232abde5874dbc9
lmonish7108 <44014424+lmonish7108@users.noreply.github.com>
2019-03-26 13:15:16 +0530

    Update tests.py
    Adding tests.

--- hosted_solution/fibonacci/tests.py
+++ hosted_solution/fibonacci/tests.py
@@ -3,4 +3,23 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
+from fibonacci import models
+from fibonacci import views
+
+
 # Create your tests here.
+class ModelCreationTestCase(TestCase):
+    def setUp(self):
+        models.FibonacciSeries.objects.create(instance_position="3", instance_value="2")
+
+    def test_record_is_created(self):
+    	create_test = models.FibonacciSeries.objects.get(instance_position="3")
+    	self.assertEqual(create_test.instance_position, "3")
+    	self.assertEqual(create_test.instance_value, "2")
+
+
+class FibonacciSeriesGenerationTestCase(TestCase):
+
+	def test_series_generation(self):
+		self.assertEqual("8", views.generateFibonacciSeries("6"))
+		self.assertEqual("2", views.generateFibonacciSeries("3"))
